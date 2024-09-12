8a9273f694d2df33e1f898c581c85ed82553a304
Seaji <135226915+Seaji06@users.noreply.github.com>
2024-09-13 00:08:35 +0800

    Update urls.py

--- AppCodingNexus/urls.py
+++ AppCodingNexus/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('verify_otp/', views.verify_otp, name='verify_otp'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
